e25812d94afa8034640d3268936ba49455ff1e7d
S4M04NSL4Y3R <S4M04NSL4Y3R@gmail.com>
2023-09-13 23:50:32 -0600

    Ignore files that exceed github's 100mb limit
    Former-commit-id: 3f4c337e73d73d62df3eb34a84e7a3e7827a9fb3

--- #kokonutkrew/warcraftlogs-overwolf/Overwolf/0.176.87.27/Resources/uninstaller/Files/js/events.js
+++
@@ -1,6 +0,0 @@
-window.document.addEventListener("DOMContentLoaded", function() {
-
-  var header = document.getElementById("app-header");
-  header.ondragstart = Commands.startDragging;
-  header.onselectstart = Commands.startDragging;
-});

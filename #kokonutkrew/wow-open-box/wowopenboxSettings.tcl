e73b8025fb19a20f9dd92fa84befc24642b5dba6
S4M04NSL4Y3R <S4M04NSL4Y3R@gmail.com>
2023-10-10 11:47:02 -0600

    Update gitignore for files under #kokonutkrew

---
+++ #kokonutkrew/wow-open-box/wowopenboxSettings.tcl
@@ -0,0 +1,90 @@
+array set settings {
+	DEBUG	0
+	aspectRatio	Any
+	autoCapture	1
+	autoKillName	WowVoiceProxy.exe
+	autoKillOn	1
+	autoResetDirect	0
+	autoResetFocusToMain	1
+	avoidTaskbar	1
+	borderless	1
+	captureForegroundWindow	0
+	clipboardAtStart	0
+	dontCaptureList	{explorer.exe SndVol.exe}
+	focusAlsoFG	1
+	game	{World of Warcraft}
+	games	{"World of Warcraft" "Overwatch" "EVE" "Star Wars\u2122: The Old Republic\u2122"}
+	hk,capture	Ctrl-Shift-C
+	hk,focusFollowMouse	Ctrl-Shift-F
+	hk,focusMain	Ctrl-Shift-W
+	hk,focusNextWindow	Ctrl-Shift-N
+	hk,focusPreviousWindow	Ctrl-Shift-P
+	hk,mouseTrack	Ctrl-Shift-M
+	hk,overlayToggle	Ctrl-Shift-O
+	hk,resetAll	Ctrl-Shift-Alt-R
+	hk,rrToggle	Ctrl-Shift-R
+	hk,stayOnTopToggle	Ctrl-Shift-T
+	hk,swapNextWindow	Ctrl-0xC0
+	hk,swapPreviousWindow	Ctrl-Shift-0xC0
+	hk1,focus	Ctrl-F1
+	hk1,swap	Ctrl-Shift-F1
+	hk10,focus	Ctrl-F10
+	hk10,swap	Ctrl-Shift-F10
+	hk2,focus	Ctrl-F2
+	hk2,swap	Ctrl-Shift-F2
+	hk3,focus	Ctrl-F3
+	hk3,swap	Ctrl-Shift-F3
+	hk4,focus	Ctrl-F4
+	hk4,swap	Ctrl-Shift-F4
+	hk5,focus	Ctrl-F5
+	hk5,swap	Ctrl-Shift-F5
+	hk6,focus	Ctrl-F6
+	hk6,swap	Ctrl-Shift-F6
+	hk7,focus	Ctrl-F7
+	hk7,swap	Ctrl-Shift-F7
+	hk8,focus	Ctrl-F8
+	hk8,swap	Ctrl-Shift-F8
+	hk9,focus	Ctrl-F9
+	hk9,swap	Ctrl-Shift-F9
+	ignoreMonitorIdx	0
+	lastUpdateChecked	1696958416
+	layoutAuto	1
+	layoutMaxWindows	32
+	layoutOneRowCol	0
+	layoutOneSize	1
+	layoutScale	1/6
+	layoutSnap	4
+	layoutStacked	0
+	layoutTop	0
+	mainWindowGeometry	225x438+449+159
+	mfIndicator,label	🐭
+	mouseDelay	0
+	mouseFocusOffAtExit	1
+	mouseFocusOnAtStart	0
+	mouseInsideGameWindowFocuses	0
+	mouseOutsideWindowsPauses	1
+	mouseWatchInterval	50
+	numWindows	0
+	overlayAllClickable	0
+	overlayAlpha	0.7
+	overlayAnchor	n
+	overlayClickable	1
+	overlayFocusColor	{#ff0080}
+	overlayFontSize1	48
+	overlayFontSize2	48
+	overlayShowBorder	1
+	profile	Default
+	profiles	{"Default"}
+	rrAlwaysFocus	1
+	rrCustomExcludeList	0
+	rrIndicator,label	⟳
+	rrIndicator,x	0.5
+	rrIndicator,y	0.7
+	rrInterval	5
+	rrKeyListAll	{SPACE 1 2 3 4 5 6 7 8 9 0 - =}
+	rrKeyListCustom	{. [ ] F11}
+	rrKeyListDirect	{.m .1 .2 .3 ...}
+	rrModExcludeList	{RCONTROL RSHIFT}
+	showOverlay	1
+	swapAlsoFocus	1
+}
